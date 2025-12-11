10b782fb6699eaea904cd2763480f65b219e62e2
Richard L <Stork66722@gmail.com>
2025-12-11 15:13:07 -0600

    Add customer templates to DIRS for password reset HTML emails

--- investr/settings.py
+++ investr/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'investr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'customer' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
